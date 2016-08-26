3368c95f06ce87f010576fdf6e428f317a800467
=chetz <=chethana.sathyanarayana@gmail.com>
2016-08-26 11:50:21 +0530

    first commit

---
+++ MyDairy/src/main/java/com/mydairy/MyDairyApplication.java
@@ -0,0 +1,12 @@
+package com.mydairy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDairyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDairyApplication.class, args);
+	}
+}
